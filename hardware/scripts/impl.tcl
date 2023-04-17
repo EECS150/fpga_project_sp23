9c8aaf27780bf9e1d60c84440d6f95370c3fdd7d
Class Account <eecs151-laa@c111-17.eecs.berkeley.edu>
2023-04-17 09:29:08 -0700

    utilization after everything

--- hardware/scripts/impl.tcl
+++ hardware/scripts/impl.tcl
@@ -9,7 +9,6 @@ if {[string trim ${CONSTRAINTS}] ne ""} {
 opt_design
 place_design
 write_checkpoint -force ${TOP}_placed.dcp
-report_utilization -file post_place_utilization.rpt
 phys_opt_design
 route_design
 
@@ -17,6 +16,7 @@ write_checkpoint -force ${TOP}_routed.dcp
 write_verilog -force post_route.v
 write_xdc -force post_route.xdc
 report_drc -file post_route_drc.rpt
+report_utilization -file post_place_utilization.rpt
 report_timing_summary -warn_on_violation -file post_route_timing_summary.rpt
 
 write_bitstream -force ${TOP}.bit
